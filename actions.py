7b269ced8e98945bdf4115dee67d89f79147b5f4
sahibpreet <ss9334931@gmail.com>
2020-11-02 09:50:25 +0530

    Made actions in actions.py

--- actions.py
+++ actions.py
@@ -7,21 +7,24 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionHullRepair(Action):
+
+    def name(self) -> Text:
+        return "action_hull_repair"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        boat_part=str(tracker.get_slot('boat_part'))
+
+
+        dispatcher.utter_message(text="Ya we can help you with %s"%(boat_part))
+
+        return [SlotSet("boat_part", None)]
